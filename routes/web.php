0769b5c434c01af3bc24dc0ccc9d5377079da706
jakiw26 <ahmadfauzandzaki@gmail.com>
2026-04-17 15:30:28 +0700

    aasdasdasdasd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,7 +11,19 @@ Route::get('/tes', function () {
     return view('tes');
 });
 
+Route::get('/user/{id}', function ($id) {
+    return "profil user dengan id:".$id;
+});
+
+Route::get('/produk/{slug?}', function ($slug = 'semua-produk') {
+    return "Menampilkan:".$slug;
+});
+
+Route::get('/guweh', function () {
+    return 'Dzaki Ahmad Fauzan';
+});
 Route::get('/guweh', function () {
     return 'Dzaki Ahmad Fauzan';
 });
 
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
